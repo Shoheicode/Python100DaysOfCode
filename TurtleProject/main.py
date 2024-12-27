23c705bd1d57f10e37adc17d2c100f8046fd201d
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-26 17:16:45 -0800

    update shapes

--- TurtleProject/main.py
+++ TurtleProject/main.py
@@ -3,23 +3,27 @@ from turtle import Screen, Turtle
 turtle_t = Turtle()
 
 # Draw square
-turtle_t.forward(120)
-turtle_t.left(90)
-turtle_t.forward(120)
-turtle_t.left(90)
-turtle_t.forward(120)
-turtle_t.left(90)
-turtle_t.forward(120)
+turtle_t.forward(50)
+turtle_t.right(90)
+turtle_t.forward(50)
+turtle_t.right(90)
+turtle_t.forward(50)
+turtle_t.right(90)
+turtle_t.forward(50)
 
 # Draw dotted line
 turtle_t.penup()
 turtle_t.forward(10)
-turtle_t.left(90)
+turtle_t.right(90)
 for i in range(10):
     turtle_t.pendown()
     turtle_t.forward(10)
     turtle_t.penup()
     turtle_t.forward(10)
+
+turtle_t.setpos(0, 0)
+
+# Draw other shapes
 
 screen = Screen()
 screen.exitonclick()
