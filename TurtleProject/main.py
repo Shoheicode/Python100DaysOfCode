d5709c635aa46e5069b7513a455ee3c08a72c46c
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-26 17:13:24 -0800

    update the dotted line

--- TurtleProject/main.py
+++ TurtleProject/main.py
@@ -10,7 +10,16 @@ turtle_t.left(90)
 turtle_t.forward(120)
 turtle_t.left(90)
 turtle_t.forward(120)
+
+# Draw dotted line
+turtle_t.penup()
+turtle_t.forward(10)
 turtle_t.left(90)
+for i in range(10):
+    turtle_t.pendown()
+    turtle_t.forward(10)
+    turtle_t.penup()
+    turtle_t.forward(10)
 
 screen = Screen()
 screen.exitonclick()
