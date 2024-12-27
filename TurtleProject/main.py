6dbcf521a04b3f10947574d8dca21ca4123393a1
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-26 17:09:54 -0800

    update square

--- TurtleProject/main.py
+++ TurtleProject/main.py
@@ -2,7 +2,15 @@ from turtle import Screen, Turtle
 
 turtle_t = Turtle()
 
+# Draw square
 turtle_t.forward(120)
+turtle_t.left(90)
+turtle_t.forward(120)
+turtle_t.left(90)
+turtle_t.forward(120)
+turtle_t.left(90)
+turtle_t.forward(120)
+turtle_t.left(90)
 
 screen = Screen()
 screen.exitonclick()
