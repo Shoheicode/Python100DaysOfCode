685e24ecf424cd2d7094c596e50c30a869707b9b
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-26 17:27:34 -0800

    updated to have color

--- TurtleProject/main.py
+++ TurtleProject/main.py
@@ -50,9 +50,12 @@ colours = [
 
 directions = [0, 90, 180, 270]
 
+turtle_t.setpos(0, 0)
+
 for _ in range(200):
     turtle_t.forward(30)
     turtle_t.setheading(random.choice(directions))
+    turtle_t.color = random.choice(colours)
 
 screen = Screen()
 screen.exitonclick()
