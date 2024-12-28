4587c0bb966f0fa4f8eebdcc7623767ffaa66ad0
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-28 11:55:01 -0800

    update the breath first search

--- TurtleProject/BeginnerProjects/PathFinder.py
+++ TurtleProject/BeginnerProjects/PathFinder.py
@@ -1,3 +1,4 @@
+import queue
 import random
 from turtle import Screen, Turtle
 
@@ -7,7 +8,7 @@ turtle_t.speed(0)
 
 def generate_maze(width, height):
     # Define the maze grid
-    maze = [[1 for _ in range(width)] for _ in range(height)]
+    maze = [[-1 for _ in range(width)] for _ in range(height)]
 
     # Define movement directions: (dx, dy)
     directions = [(0, 2), (2, 0), (0, -2), (-2, 0)]
@@ -18,8 +19,10 @@ def generate_maze(width, height):
         random.shuffle(directions)
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
-            if 0 < nx < width - 1 and 0 < ny < height - 1 and maze[ny][nx] == 1:
-                maze[y + dy // 2][x + dx // 2] = 0  # Remove wall between cells
+            if 0 < nx < width - 1 and 0 < ny < height - 1 and maze[ny][nx] == -1:
+                maze[y + dy // 2][x + dx // 2] = float(
+                    "inf"
+                )  # Remove wall between cells
                 carve(nx, ny)
 
     # Start carving from a random point
@@ -29,8 +32,8 @@ def generate_maze(width, height):
     )  # random.randrange(1, width, 2), random.randrange(1, height, 2)
     carve(start_x, start_y)
 
-    maze[1][1] = 3  # Start point
-    maze[height - 2][width - 2] = 4  # End point
+    maze[1][1] = -2  # Start point
+    maze[height - 2][width - 2] = -3  # End point
 
     return maze
 
@@ -39,11 +42,11 @@ def print_maze(maze):
     for row in maze:
         st = ""
         for cell in row:
-            if cell == 1:
+            if cell == -1:
                 st = st + "X"
-            elif cell == 3:
+            elif cell == -2:
                 st = st + "S"
-            elif cell == 4:
+            elif cell == -3:
                 st = st + "E"
             else:
                 st = st + " "
@@ -80,10 +83,18 @@ def draw_map(maze):
             # turtle_t.shape("square")
         turtle_t.penup()
 
+
 draw_map(maze)
 
-def solve_maze():
-    
+
+def solve_maze(maze):
+
+    qu = queue.Queue()
+    qu.put((1,1))
+
+    while not qu.empty():
+        
+
 
 screen = Screen()
 screen.exitonclick()
