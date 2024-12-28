44b489c2c79d11d5a6e75fa55994fc585d786e38
ShoheiCode <ucodejasonirie@gmail.com>
2024-12-28 11:18:37 -0800

    remove unecessary code

--- TurtleProject/BeginnerProjects/main.py
+++ TurtleProject/BeginnerProjects/main.py
@@ -38,29 +38,5 @@ turtle_t.pendown()
 for i in range(3, 12):
     draw_shape(i)
 
-# Drawing the color lines
-
-colours = [
-    "CornflowerBlue",
-    "DarkOrchid",
-    "IndianRed",
-    "DeepSkyBlue",
-    "LightSeaGreen",
-    "wheat",
-    "SlateGray",
-    "SeaGreen",
-]
-
-directions = [0, 90, 180, 270]
-
-turtle_t.setpos(0, 0)
-turtle_t.speed(9)
-turtle_t.pensize(10)
-
-for _ in range(200):
-    turtle_t.forward(30)
-    turtle_t.setheading(random.choice(directions))
-    turtle_t.color(random.choice(colours))
-
 screen = Screen()
 screen.exitonclick()
